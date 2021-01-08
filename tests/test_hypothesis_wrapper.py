11776a884b33aff240f3b832ad47c8b9e22ec4a3
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-08 10:23:39 -0500

    Updating test to use local function rather than relying on package from github

--- tests/test_hypothesis_wrapper.py
+++ tests/test_hypothesis_wrapper.py
@@ -1,19 +1,30 @@
 """Democritus functions to interact with Hypothesis."""
 
-from hypothesis.strategies import dates
-from democritus_dates import is_date
+import ipaddress
+
+from hypothesis.provisional import ip4_addr_strings
 
 from democritus_hypothesis import hypothesis_get_strategy_results
 
 
+def is_ip_address(text: str) -> bool:
+    """Determine whether or not the given text is an ip address."""
+    try:
+        ipaddress.ip_address(text)
+    except ValueError:
+        return False
+    else:
+        return True
+
+
 def test_hypothesis_get_strategy_results_dates():
-    results = hypothesis_get_strategy_results(dates)
+    results = hypothesis_get_strategy_results(ip4_addr_strings)
     assert len(results) == 10
     for result in results:
-        assert is_date(result)
+        assert is_ip_address(result)
 
     # try specifying a specific number
-    results = hypothesis_get_strategy_results(dates, n=12)
+    results = hypothesis_get_strategy_results(ip4_addr_strings, n=12)
     assert len(results) == 12
     for result in results:
-        assert is_date(result)
+        assert is_ip_address(result)

2180b5d011de1a8364afb279d83f3993d2ffa114
Floyd Hightower <floyd.hightower27@gmail.com>
2021-01-21 08:57:28 -0500

    Improving version management
    See https://github.com/democritus-project/roadmap/issues/4

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '2021.01.08'
-
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
@@ -15,7 +13,6 @@ with open('requirements_dev.txt') as requirements_dev_file:
 
 setup(
     name='democritus_hypothesis',
-    version=__version__,
     description="Democritus functions to interact with Hypothesis.",
     long_description=readme,
     long_description_content_type="text/markdown",
